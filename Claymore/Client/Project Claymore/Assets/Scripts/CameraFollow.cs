1c47a51c41c3eec18ffb92244be8a757a549802e
Sorae <System.NullReferenceException@gmail.com>
2017-06-06 22:35:13 +0900

    * implemented basic moving and camera controller

---
+++ Claymore/Client/Project Claymore/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace CompleteProject
+{
+    public class CameraFollow : MonoBehaviour
+    {
+		[SerializeField]
+        private Transform _target = null;            // The position that that camera will be following.
+
+		[SerializeField]
+		private Vector2 _offset = Vector2.zero;
+
+		[SerializeField]
+		private Vector3 _defaultPlayerPositionInScreen = new Vector3(0.5f,0.5f,0.0f);
+
+		public void LateUpdate()
+		{
+			var mousePosition = Vector3.Min(Vector3.one, Camera.main.ScreenToViewportPoint(Input.mousePosition));
+			var worldDistance = Camera.main.ViewportToWorldPoint(mousePosition) -  Camera.main.ViewportToWorldPoint(_defaultPlayerPositionInScreen);
+
+			worldDistance.x *= (1.0f -_offset.x);
+			worldDistance.y *= (1.0f -_offset.y);
+
+			var cameraPosition = _target.position + worldDistance;
+			cameraPosition.z = -99;
+
+			transform.localPosition = cameraPosition;
+		}
+	}
+}
---
+++ Claymore/Client/Project Claymore/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+	[SerializeField]
+	private float _moveSpeed = 0.1f;
+
+	[SerializeField]
+	private Rigidbody2D _rigidBody = null;
+
+
+	public void FixedUpdate()
+	{
+		handlePlayerMove();
+		//if (Input.GetKeyDown(KeyCode.A))
+			handlePlayerRotation();
+	}
+	
+	private void handlePlayerRotation()
+	{
+		var mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		var playerToMouse = mousePos - transform.position;
+		var angleInRadius = Mathf.Atan2(playerToMouse.y, playerToMouse.x);
+		var angleInDegree = (180 / Mathf.PI) * angleInRadius;
+		_rigidBody.MoveRotation(angleInDegree);
+
+	}
+
+	private void handlePlayerMove()
+	{
+		var playerDirection = Vector2.zero;
+
+		if (Input.GetKey(KeyCode.W))
+			playerDirection += Vector2.up;
+		if (Input.GetKey(KeyCode.S))
+			playerDirection += Vector2.down;
+		if (Input.GetKey(KeyCode.D))
+			playerDirection += Vector2.right;
+		if (Input.GetKey(KeyCode.A))
+			playerDirection += Vector2.left;
+
+		var moveVector = playerDirection.normalized;
+
+		_rigidBody.MovePosition(_rigidBody.position + moveVector * _moveSpeed);
+	}
+}
